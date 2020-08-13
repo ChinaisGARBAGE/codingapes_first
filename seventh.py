8e68c1d6f66cc3fe0982827f6a759c21234ae360
ChinaisGARBAGE <69445858+ChinaisGARBAGE@users.noreply.github.com>
2020-08-13 16:49:36 +0800

    Add files via upload

---
+++ seventh.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 13 16:20:53 2020
+
+@author: USER
+"""
+A=10
+B=24
+C=31
+D=45
+if A>B:
+    T=A
+    A=B
+    B=T
+if B>C:
+    T=B
+    B=C
+    C=T
+if A>B :
+    T = A
+    A = B
+    B = T
+print(A,B,C)
